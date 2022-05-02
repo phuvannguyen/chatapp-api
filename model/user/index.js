c176a80ee1fccd2a8e4fb4244204141ad5f8d1da
unknown <phu21994@gmail.com>
2022-05-02 20:40:17 +0700

    Create User Model

---
+++ model/user/index.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {//type: String
+        type: String,
+        index: true,
+        unique: true,
+        sparse: true
+    },
+    email: {//type: String
+        type: String,
+        index: true,
+        unique: true,
+        sparse: true
+    },
+    password: String,
+    passwordResetToken: String, // when user forget password, will send a token. Then user type it, can change forget password.
+    passwordResetExpires: Date, // time limit token.
+    github: {//GitHub’s profile id
+        type: String,        
+        unique: true,
+        sparse: true
+    },    
+    google: {//Google’s profile id
+        type: String,        
+        unique: true,
+        sparse: true
+    },
+
+    profile: {
+        name: String,
+        avatar_url: String,
+        location: String,
+        status: String,
+        gender: String
+
+    },
+    tokens: [{//: list of linked services tokens
+        _id: false,
+        service: String, // service name (i.e. github)
+        accessToken: String//access token given by the service. 
+
+    }
+
+    ]  
+
+
+
+}, {timestamps: true});
+
+const Users = mongoose.model("users", userSchema);
+
+export default Users;
