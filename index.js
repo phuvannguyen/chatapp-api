0c626c4c1fd6da99b7e61a74e5815ade2f8029c3
unknown <phu21994@gmail.com>
2022-05-02 14:29:49 +0700

    Start setup for server
    Suck port, simple router, connect database

---
+++ index.js
@@ -0,0 +1,50 @@
+import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import dotenv from 'dotenv'
+import cors from "cors"
+import http from "http"
+
+
+//app config
+const app = express();
+dotenv.config();
+const port = process.env.PORT
+
+const corsOptions = {
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+}
+app.use(cors(corsOptions))
+
+//middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+const server = http.createServer(app)
+
+//DB config
+const url = "mongodb+srv://phu1994:6298327@cluster0.rpsig.mongodb.net/message?retryWrites=true"
+
+try {
+    await mongoose.connect(url);
+    console.log("Connected with Mongodb")
+  } catch (error) {
+    console.log(error);
+    console.log("No connected with Mongodb")
+  }
+
+//router
+
+app.get("/", (req, res) => {
+    res.send("Hello world. This is Phu Nguyen")
+    
+})
+
+
+
+
+server.listen(port, () => {
+    console.log("Hi port:" + port)
+})
+
