44bee4961aea404731c613312961355b55e7a72b
unknown <phu21994@gmail.com>
2022-05-30 15:13:10 +0700

    Add token middleware

---
+++ middleware/index.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+export const authenToken = (req, res, next) => {
+    const authHeader = req.headers["authorization"];    
+    
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token == null) {
+      
+      return res.status(403).send({ message: "No token provided!" });
+    };
+
+    jwt.verify(token, process.env.ACCESS_SECRET, (err) => {
+      if (err) {
+        console.log("THis is token", token)
+        res.status("403")        
+      };
+      req.user = req.body;    
+
+    })
+
+    next()
+}
