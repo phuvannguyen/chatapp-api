74afcaec17e50f23627b3db1cd9ff6c05536ea7e
unknown <phu21994@gmail.com>
2022-07-06 14:07:08 +0700

    Add User router and chat router

--- router/index.js
+++ router/index.js
@@ -4,8 +4,22 @@ import { isRoomIdValid, isRoomOwner } from "../middleware/room.js";
 import Users from "../model/user/index.js";
 import {login, registation} from "../controllers/auth.js"
 import { getRooms, postRooms, filterRoom, getOneRoom, editRoom, joinRoom, leaveRoom, deleteRoom } from "../controllers/room.js";
+import { chats, createChat, deleteChat, editChat, getChat } from "../controllers/chat.js";
+import { isChatIdValid } from "../middleware/chat.js";
+import { findUser, users } from "../controllers/user.js";
+import { isUserValid } from "../middleware/user.js";
 //app config
 const router = express.Router();
+
+/**
+ * Users
+ */
+ router.get("/api/" + 'users', users); 
+ router.get("/api/" + 'users/:_id', isUserValid, findUser);
+ 
+ // User -> Room. 
+//  router.get("/api/" + 'users/:_id/rooms/owned', isUserValid, userOwner);
+ 
 
 //router for room
     //List of rooms
@@ -22,6 +36,17 @@ router.patch("/api/" + 'rooms/:_id', authenToken, isRoomIdValid, isRoomOwner, ed
 router.post("/api/" + 'rooms/:_id/join', authenToken, isRoomIdValid, joinRoom);
 router.post("/api/" + 'rooms/:_id/leave', authenToken, isRoomIdValid, leaveRoom);
 router.delete('/api/' + 'rooms/:_id', authenToken, isRoomIdValid, isRoomOwner, deleteRoom);
+
+// Room -> Chat.
+router.get('/api/' + 'rooms/:_id/chats', isRoomIdValid, chats);
+router.post('/api/' + 'rooms/:_id/chats', authenToken, isRoomIdValid, isRoomOwner, createChat);
+
+/**
+ * Chats
+ */
+router.get('/api/' + 'chats/:_id', isChatIdValid, getChat);
+router.patch('/chats/:_id', isChatIdValid, editChat);
+router.delete('/chats/:_id', authenToken, isChatIdValid, deleteChat)
 
 router.get("/", authenToken, (req, res) => {
     res.send("Hello world. This is Phu Nguyen")
